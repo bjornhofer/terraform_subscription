7168e97cf6131a42898456bcba1b112ae156ce42
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-02-06 16:19:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+provider "azurerm" {
+  features {}
+  alias = "subscription"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+} 
 
 data "azurerm_billing_enrollment_account_scope" "main" {
   count                   = var.dbg_simulate == true ? 0 : 1
   billing_account_name    = var.billing_account_name
   enrollment_account_name = var.enrollment_account_name
+  provider = azurerm.subscription
 }
 
 resource "azurerm_subscription" "main" {
@@ -10,9 +19,11 @@ resource "azurerm_subscription" "main" {
   alias             = var.subscription_alias
   subscription_name = var.subscription_name
   billing_scope_id  = data.azurerm_billing_enrollment_account_scope.main[0].id
+  provider = azurerm.subscription
 }
 
 // DEBUG - Query current subscription, for output values
 data "azurerm_subscription" "current" {
   count = var.dbg_simulate == true ? 1 : 0
+  provider = azurerm.subscription
 }
