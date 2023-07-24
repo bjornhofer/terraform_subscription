db95ab4be191be9452b59a77c585710e666a5bb9
Björn Andreas Höfer <bjoern.hoefer@microsoft.com>
2023-07-24 15:27:46 +0200

    added prefix and suffix option

--- main.tf
+++ main.tf
@@ -1,29 +1,32 @@
 provider "azurerm" {
   features {}
-  alias = "subscription"
+  alias           = "subscription"
   subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
-} 
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
 
 data "azurerm_billing_enrollment_account_scope" "main" {
-  count                   = var.dbg_simulate == true ? 0 : 1
+  count                   = var.subscription_create == true ? 1 : 0
   billing_account_name    = var.billing_account_name
   enrollment_account_name = var.enrollment_account_name
-  provider = azurerm.subscription
+  provider                = azurerm.subscription
+}
+
+locals {
+  subscription_name = length(var.subscription_name_suffix) > 0 ? "${var.subscription_name_prefix}-${var.subscription_name_suffix}" : var.subscription_name_prefix
 }
 
 resource "azurerm_subscription" "main" {
-  count             = var.dbg_simulate == true ? 0 : 1
+  count             = var.subscription_create == true ? 1 : 0
   alias             = var.subscription_alias
-  subscription_name = var.subscription_name
+  subscription_name = local.subscription_name
   billing_scope_id  = data.azurerm_billing_enrollment_account_scope.main[0].id
-  provider = azurerm.subscription
+  provider          = azurerm.subscription
 }
 
-// DEBUG - Query current subscription, for output values
 data "azurerm_subscription" "current" {
-  count = var.dbg_simulate == true ? 1 : 0
+  count    = var.subscription_create == false ? 1 : 0
   provider = azurerm.subscription
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "subscription" {
-  value = var.dbg_simulate == true ? data.azurerm_subscription.current[0] : resource.azurerm_subscription.main[0]
+  value = var.subscription_create == false ? data.azurerm_subscription.current[0] : resource.azurerm_subscription.main[0]
 }
