13b7f2f74aa1259868c4be13116e90d9f63f5354
Björn Andreas Höfer <bjoern.hoefer@microsoft.com>
2023-07-25 10:15:13 +0200

    linting

--- main.tf
+++ main.tf
@@ -27,11 +27,11 @@ resource "azurerm_subscription" "main" {
 }
 
 resource "azurerm_management_group_subscription_association" "subscription_assignment" {
-  count              = var.subscription_create == true && length(var.management_group_id) > 0 ? 1 : 0
+  count               = var.subscription_create == true && length(var.management_group_id) > 0 ? 1 : 0
   management_group_id = var.management_group_id
   subscription_id     = azurerm_subscription.main[0].id
   provider            = azurerm.subscription
-  depends_on = [ azurerm_subscription.main ]
+  depends_on          = [azurerm_subscription.main]
 }
 
 data "azurerm_subscription" "current" {
